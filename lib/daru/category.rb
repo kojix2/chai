27e0482712553eb56c48b3c073626d8d65467c22
kojix2 <2xijok@gmail.com>
2019-09-26 21:50:49 +0900

    Remove all plot methods

--- lib/daru/category.rb
+++ lib/daru/category.rb
@@ -59,28 +59,6 @@ module Daru
       self
     end
 
-    def plotting_library= lib
-      case lib
-      when :gruff
-        @plotting_library = lib
-        if Daru.send("has_#{lib}?".to_sym)
-          extend Module.const_get(
-            "Daru::Plotting::Category::#{lib.to_s.capitalize}Library"
-          )
-        end
-      else
-        raise ArgumentError, "Plotting library #{lib} not supported. "\
-          'Supported libraries is :gruff'
-      end
-    end
-
-    # this method is overwritten: see Daru::Category#plotting_library=
-    def plot(*args, **options, &b)
-      init_plotting_library
-
-      plot(*args, **options, &b)
-    end
-
     alias_method :rename, :name=
 
     # Returns an enumerator that enumerates on categorical data
@@ -754,11 +732,6 @@ module Daru
     end
 
     private
-
-    # Will lazily load the plotting library being used
-    def init_plotting_library
-      self.plotting_library = Daru.plotting_library
-    end
 
     def validate_categories input_categories
       raise ArgumentError, 'Input categories and speculated categories mismatch' unless
--- lib/daru/plotting/gruff.rb
+++
@@ -1,3 +0,0 @@
-require_relative 'gruff/category.rb'
-require_relative 'gruff/vector.rb'
-require_relative 'gruff/dataframe.rb'
